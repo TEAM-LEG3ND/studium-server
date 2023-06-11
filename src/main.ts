4ceb47148b374e10ca94bae2bd70683b7301decf
redjen8 <seokwoo2525@gmail.com>
2023-06-11 23:30:28 +0900

    Feat: add winston logger module and config

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,37 @@ import { ValidationPipe, INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { WinstonModule } from 'nest-winston';
+import { format, transports } from 'winston';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+
+        new transports.File({
+          filename: `logs/every.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            })
+          )
+        })
+      ]
+    })
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true, //ignore unknow properties
     forbidNonWhitelisted:true,
