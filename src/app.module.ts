70509bba98b718188127a5ae59d94b634beb84aa
redjen8 <seokwoo2525@gmail.com>
2023-06-18 23:35:03 +0900

    Feat: logger module test added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { RecruitArticleModule } from './recruit-article/recruit-article.module';
 import { AppController } from './app.controller';
-import { AppService  } from './app.service';
+import { AppService } from './app.service';
 
 @Module({
   imports: [RecruitArticleModule],
   controllers: [AppController],
-  providers: [AppService ],
+  providers: [AppService, Logger],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
-
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
+import { Logger } from '@nestjs/common/services';
 @Injectable()
 export class AppService {
+  constructor(@Inject(Logger) private readonly logger: LoggerService) {}
+
   getHello(): string {
+    this.logger.warn('hello world!');
     return 'Hello World!';
   }
 }
