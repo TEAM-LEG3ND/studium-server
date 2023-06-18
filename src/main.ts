56fcadb4a6a536a4f111042219ba2412a3cf279e
redjen8 <seokwoo2525@gmail.com>
2023-06-18 23:34:45 +0900

    Feat: separated logger config instance

--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WinstonModule } from 'nest-winston';
-import { format, transports } from 'winston';
 import { initSwaggerConfig } from './config/swagger-config';
+import { initWinstonConfigInstance } from './config/winston-logger-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: WinstonModule.createLogger({
-      transports: [
-        new transports.File({
-          filename: `logs/error.log`,
-          level: 'error',
-          format: format.combine(format.timestamp(), format.json()),
-        }),
-
-        new transports.File({
-          filename: `logs/every.log`,
-          format: format.combine(format.timestamp(), format.json()),
-        }),
-
-        new transports.Console({
-          format: format.combine(
-            format.cli(),
-            format.splat(),
-            format.timestamp(),
-            format.printf((info) => {
-              return `${info.timestamp} ${info.level}: ${info.message}`;
-            })
-          )
-        })
-      ]
-    })
+    logger: WinstonModule.createLogger(initWinstonConfigInstance()),
   });
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist:true, //ignore unknow properties
-    forbidNonWhitelisted:true,
-    transform:true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //ignore unknow properties
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   //api docs config
   initSwaggerConfig(app);
