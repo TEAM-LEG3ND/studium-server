9c90729d0fb2f42a263f0f168d1361a845868f13
selgyun <kindkiz@naver.com>
2023-06-22 20:52:10 +0900

    feat: Apply router-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    RouterModule.register([
+      // {
+      //   path: 'sample-path',
+      //   module: SampleModule,
+      // }
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
