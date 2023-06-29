43b8b00bbb1b3e9f80277c65d3709d233d07f49c
selgyun <kindkiz@naver.com>
2023-06-29 16:46:39 +0900

    feat: Add create-user dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Get()
-    getAll() {
-        return this.userService.getAll();
+    findAll() {
+        return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.userService.findOne(+id);
+    }
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto ) {
+        return this.userService.create(createUserDto);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
     constructor(private prisma: PrismaService) {}
 
-    async getAll() {
+    async findAll() {
         return await this.prisma.user.findMany();
+    }
+
+    async findOne(id: number) {
+        return await this.prisma.user.findUnique({where: {id}});
+    }
+
+    async create(createUserDto: CreateUserDto) {
+        return await this.prisma.user.create({
+            data: {
+                ...createUserDto,
+            },
+        });
     }
 }
