4a6d5bf86fb7a50f0c01b7438e54a6bcf6cb561e
selgyun <kindkiz@naver.com>
2023-07-03 11:10:20 +0900

    feat: Add update & delete api, handle exception

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -11,7 +12,13 @@ export class UserService {
     }
 
     async findOne(id: number) {
-        return await this.prisma.user.findUnique({where: {id}});
+        const user = await this.prisma.user.findUnique({
+            where: { id },
+        });
+        if (!user) {
+            throw new NotFoundException(`User with ID: ${id} not Found.`);
+        }
+        return user;
     }
 
     async create(createUserDto: CreateUserDto) {
@@ -19,6 +26,21 @@ export class UserService {
             data: {
                 ...createUserDto,
             },
+        });
+    }
+
+    async update(id: number, updateUserDto: UpdateUserDto) {
+        const user = await this.findOne(id);
+        return await this.prisma.user.update({
+            where: { id: user['id'] },
+            data: updateUserDto,
+        });
+    }
+
+    async remove(id: number) {
+        const user = await this.findOne(id);
+        return await this.prisma.user.delete({
+            where: { id: user['id'] },
         });
     }
 }
