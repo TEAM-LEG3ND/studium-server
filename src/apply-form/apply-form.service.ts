c936ed2835b5e8d0c8cfcbab4321f041cce57738
sw-jeong <sw.jeong@navercorp.com>
2023-07-10 18:01:38 +0900

    Feat: crud 메서드 추가

--- src/apply-form/apply-form.service.ts
+++ src/apply-form/apply-form.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { CreateApplyFormDto } from './dto/create-apply-form.dto';
 
 @Injectable()
-export class ApplyFormService {}
+export class ApplyFormService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    return await this.prisma.applyForm.findMany();
+  }
+
+  async findById(id: number) {
+    const applyForm = await this.prisma.applyForm.findUnique({
+      where: { id },
+      include: { user: true, study: true },
+    });
+    return applyForm;
+  }
+
+  async create(data: CreateApplyFormDto) {
+    const applyForm = await this.prisma.applyForm.create({
+      data,
+      include: { user: true, study: true },
+    });
+    return applyForm;
+  }
+
+  async update(id: number, data: CreateApplyFormDto) {
+    const applyForm = await this.prisma.applyForm.update({
+      where: { id },
+      data,
+      //   include: { user: true, study: true },
+    });
+    return applyForm;
+  }
+
+  async deleteById(id: number) {
+    const applyForm = await this.findById(id);
+    await this.prisma.applyForm.delete({ where: { id } });
+    return applyForm;
+  }
+}
