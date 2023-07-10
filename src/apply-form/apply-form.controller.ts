4184fcc46b1107d3bc6dd871a91bc86b69a20192
sw-jeong <sw.jeong@navercorp.com>
2023-07-10 18:01:52 +0900

    Feat: dto, controller 추가

--- src/apply-form/apply-form.controller.ts
+++ src/apply-form/apply-form.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApplyFormService } from './apply-form.service';
 
 @Controller()
-export class ApplyFormController {}
+export class ApplyFormController {
+  constructor(private readonly applyFormService: ApplyFormService) {}
+
+  @Get()
+  findAll() {
+    return this.applyFormService.findAll();
+  }
+}
