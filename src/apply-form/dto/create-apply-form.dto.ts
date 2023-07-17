c6cd4175cd052b95a01b7dd4f4d5e2df95449046
selgyun <kindkiz@naver.com>
2023-07-17 16:28:53 +0900

    fix: Fix create-apply-form dto

--- src/apply-form/dto/create-apply-form.dto.ts
+++ src/apply-form/dto/create-apply-form.dto.ts
@@ -1,1 +1,17 @@
-export class CreateApplyFormDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsNumber } from 'class-validator';
+export class CreateApplyFormDto {
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    readonly userId: number;
+    
+    @ApiProperty()
+    @IsOptional()
+    @IsNumber()
+    readonly studyId: number;
+
+    @ApiProperty()
+    @IsOptional()
+    readonly answer;   
+}
