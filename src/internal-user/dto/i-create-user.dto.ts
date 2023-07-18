06bce49378087890bbb1eaeee1c13be571c7d914
selgyun <kindkiz@naver.com>
2023-07-18 16:10:00 +0900

    feat: Remove unused files

--- src/internal-user/dto/i-create-user.dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
-
-export class InternalCreateUserDto{
-    @ApiProperty()
-    @IsNumber()
-    readonly universalAccountId: number;
-}
