e07005f4f745cd147c9271aa0e9027293f7bc58d
ammerss <amyss0907@gmail.com>
2023-07-27 10:27:43 -0400

    feat: update-study dto

---
+++ src/study/dto/update-study-response.dto.ts
@@ -0,0 +1,12 @@
+import { UpdateStudyDto } from './update-study.dto';
+
+export class UpdateStudyResponseDto extends UpdateStudyDto {
+  // You can add additional properties specific to the response, if needed.
+  // For example, a message or status code, but they are optional.
+
+  // constructor is optional if the superclass already has one that sets the properties
+  constructor(partial: Partial<UpdateStudyDto>) {
+    super();
+    Object.assign(this, partial);
+  }
+}
