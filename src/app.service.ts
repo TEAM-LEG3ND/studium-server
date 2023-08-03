d3ffe481de8d7ac7ab68f3b12cf3d4e6bcbb7fde
sw-jeong <sw.jeong@navercorp.com>
2023-08-03 22:57:58 +0900

    Feat: add exception example at app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,10 +1,13 @@
-import { Inject, Injectable, LoggerService } from '@nestjs/common';
+import { BadRequestException, Inject, Injectable, LoggerService } from '@nestjs/common';
 import { Logger } from '@nestjs/common/services';
+import { StudiumException } from './common/studium-exception';
+
 @Injectable()
 export class AppService {
   constructor(@Inject(Logger) private readonly logger: LoggerService) {}
 
   getHello(): string {
+    // throw new BadRequestException(StudiumException.test);
     return 'Hello Studium!';
   }
 }
