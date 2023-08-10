d8cae998ce50b06d874a31b0bf893d3ec633ef83
sw-jeong <sw.jeong@navercorp.com>
2023-08-10 21:13:36 +0900

    Refact: remove unused exception throw

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,11 @@
-import { BadRequestException, Inject, Injectable, LoggerService } from '@nestjs/common';
+import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { Logger } from '@nestjs/common/services';
-import { StudiumException } from './common/studium-exception';
 
 @Injectable()
 export class AppService {
   constructor(@Inject(Logger) private readonly logger: LoggerService) {}
 
   getHello(): string {
-    // throw new BadRequestException(StudiumException.test);
     return 'Hello Studium!';
   }
 }
