878d29dae190b2b8771bc6245087cb1cf7d6e0bc
selgyun <kindkiz@naver.com>
2023-08-24 17:28:19 +0900

    feat: Add nickname in user dtos

--- src/user/dto/get-user-response.dto.ts
+++ src/user/dto/get-user-response.dto.ts
@@ -8,6 +8,10 @@ export class GetUserResponseDto {
     readonly id: number;
 
     @ApiProperty()
+    @IsString()
+    readonly nickname: string;
+
+    @ApiProperty()
     @IsNumber()
     readonly manners: number;
 
@@ -19,14 +23,15 @@ export class GetUserResponseDto {
     @IsString()
     readonly profileURL: string;
 
-    constructor(id: number, manners: number, intro: string, profileURL: string) {
+    constructor(id: number, nickname: string, manners: number, intro: string, profileURL: string) {
         this.id = id;
+        this.nickname = nickname;
         this.manners = manners;
         this.intro = intro;
         this.profileURL = profileURL;
     }
 
     static fromUser(user: User) {
-        return new GetUserResponseDto(user.id, user.manners, user.intro, user.profileURL);
+        return new GetUserResponseDto(user.id, user.nickname, user.manners, user.intro, user.profileURL);
     }
 }
