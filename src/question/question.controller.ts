b00e0e5bcc1d8c03dbcc05e4a33814a643a0bc3f
ammerss <amyss0907@gmail.com>
2023-08-27 15:20:53 -0400

    remove comments

--- src/question/question.controller.ts
+++ src/question/question.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { QuestionService } from './question.service';
-import { CreateQuestionDto } from './dto/create-question.dto';
 
 @Controller()
 export class QuestionController {
