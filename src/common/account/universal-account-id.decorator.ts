4e77f26a5f69191d5ba68be78aa2361a1ee1b2fe
d0lim <limdongyoung0@gmail.com>
2023-09-07 22:34:38 +0900

    refactor: modify name to UniversalAccountIdHeader and introduce UniversalAccountId type

--- src/common/account/universal-account-id.decorator.ts
+++ src/common/account/universal-account-id.decorator.ts
@@ -1,7 +1,11 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+export type UniversalAccountId = string | null | undefined;
+
 export const X_ACCOUNT_ID_HEADER = 'x-account-id';
 
-export const UniversalAccountId = createParamDecorator((data, ctx: ExecutionContext): string | null | undefined => {
-  return ctx.switchToHttp().getRequest().headers[X_ACCOUNT_ID_HEADER];
-});
+export const UniversalAccountIdHeader = createParamDecorator(
+  (data, ctx: ExecutionContext): string | null | undefined => {
+    return ctx.switchToHttp().getRequest().headers[X_ACCOUNT_ID_HEADER];
+  },
+);
