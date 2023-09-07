8aabb971fac474c5b331db973479cf9b80634657
d0lim <limdongyoung0@gmail.com>
2023-09-07 22:35:21 +0900

    feat: apply UniversalAccountId to app controller and service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,12 @@
 import { Inject, Injectable, LoggerService } from '@nestjs/common';
 import { Logger } from '@nestjs/common/services';
+import { UniversalAccountId } from './common/account/universal-account-id.decorator';
 
 @Injectable()
 export class AppService {
   constructor(@Inject(Logger) private readonly logger: LoggerService) {}
 
-  getHello(): string {
-    return 'Hello Studium!';
+  getHello(accountId: UniversalAccountId): string {
+    return `Hello Studium! and Hello user with id ${accountId}`;
   }
 }
