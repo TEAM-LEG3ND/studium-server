f8ccc80d0168157f8ed00408352f6a9e567ed19d
D0Lim <limdongyoung0@gmail.com>
2023-09-08 17:53:03 +0900

    [feat] API Gateway Auth 적용 (#77)
    * feat: implement UniversalAccountId decorator for parsing x-account-id
    
    * feat: implement SwaggerUniversalAccountId decorator for not public endpoints
    
    * refactor: modify name to UniversalAccountIdHeader and introduce UniversalAccountId type
    
    * feat: apply UniversalAccountId to app controller and service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UniversalAccountId, UniversalAccountIdHeader } from './common/account/universal-account-id.decorator';
+import { SwaggerUniversalAccountId } from './config/decorator/swagger-universal-account-id.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @SwaggerUniversalAccountId()
+  getHello(@UniversalAccountIdHeader() accountId: UniversalAccountId): string {
+    return this.appService.getHello(accountId);
   }
 }
